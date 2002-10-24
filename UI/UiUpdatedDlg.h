32e962d92326e20a7333877c0efca5dbf620703f
Alexander Grigoriev <alegr@earthlink.net>
2002-10-23 20:44:13 -0700

    Added UI/UiUpdatedDlg.cpp, UI/UiUpdatedDlg.h

---
+++ UI/UiUpdatedDlg.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+// CUiUpdatedDlg dialog
+
+class CUiUpdatedDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CUiUpdatedDlg)
+
+public:
+	CUiUpdatedDlg(UINT Id, CWnd* pParent = NULL);   // standard constructor
+	virtual ~CUiUpdatedDlg();
+
+// Dialog Data
+
+protected:
+	void NeedUpdateControls()
+	{
+		m_bNeedUpdateControls = TRUE;
+	}
+
+	afx_msg LRESULT OnKickIdle(WPARAM, LPARAM);
+
+	DECLARE_MESSAGE_MAP()
+private:
+	BOOL m_bNeedUpdateControls;
+};
