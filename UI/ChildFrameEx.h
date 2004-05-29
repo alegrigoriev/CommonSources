e9fe9b8c5493529ae06fa9d90b79feb0751a28d9
Alexander Grigoriev <alegr@earthlink.net>
2004-05-28 21:27:11 -0700

    Generic support for preserving child frame maximized state

---
+++ UI/ChildFrameEx.h
@@ -0,0 +1,101 @@
+#pragma once
+#include "MessageMapT.h"
+
+// MainFrameExT template
+enum ChildFrameFeatures
+{
+	ChildFrameRememberMaximized = 1,
+};
+template<int _Feature = ChildFrameRememberMaximized,
+		class Base = CMDIChildWnd>
+class ChildFrameExParameters
+{
+public:
+	typedef Base BaseClass;
+	enum { Feature = _Feature};
+};
+
+typedef ChildFrameExParameters<> DefaultChildFrameExParameters;
+
+template<class Parameters = DefaultChildFrameExParameters>
+class CChildFrameExT : public Parameters::BaseClass
+{
+	typedef typename Parameters::BaseClass BaseClass;
+	enum { Feature = Parameters::Feature };
+
+public:
+protected:
+	CChildFrameExT()
+	{
+		if (Feature & ChildFrameRememberMaximized)
+		{
+			static bool IsAddedToProfile = false;
+			if ( ! IsAddedToProfile)
+			{
+				GetApp()->Profile.AddItem(_T("Settings"), _T("OpenChildMaximized"), m_bOpenChildMaximized, true);
+				IsAddedToProfile = true;
+			}
+		}
+	}
+
+	~CChildFrameExT()
+	{
+	}
+// Overrides
+	// ClassWizard generated virtual function overrides
+	////{{AFX_VIRTUAL(CChildFrameExT)
+protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs)
+	{
+
+		if( ! BaseClass::PreCreateWindow(cs) )
+		{
+			return FALSE;
+		}
+
+		if (Feature & ChildFrameRememberMaximized)
+		{
+			CMDIChildWnd * pActive = ((CMDIFrameWnd *)AfxGetMainWnd())->MDIGetActive();
+
+			if ((pActive == NULL && m_bOpenChildMaximized)
+				|| (pActive != NULL && (WS_MAXIMIZE & pActive->GetStyle())))
+			{
+				if (0 == cs.style)
+				{
+					cs.style = WS_CHILD | WS_VISIBLE | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU
+								| FWS_ADDTOTITLE | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+				}
+				cs.style |= WS_MAXIMIZE;
+			}
+		}
+		return TRUE;
+	}
+
+	////}}AFX_VIRTUAL
+private:
+	static bool m_bOpenChildMaximized;
+
+// Generated message map functions
+protected:
+	afx_msg void OnDestroy()
+	{
+		if (Feature & ChildFrameRememberMaximized)
+		{
+			WINDOWPLACEMENT wp;
+			wp.length = sizeof wp;
+
+			GetWindowPlacement( & wp);
+			m_bOpenChildMaximized = 0 != (wp.flags & WPF_RESTORETOMAXIMIZED);
+		}
+		BaseClass::OnDestroy();
+	}
+	////}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+BEGIN_MESSAGE_MAP_T(CChildFrameExT, BaseClass)
+END_MESSAGE_MAP()
+
+template<class Parameters> bool CChildFrameExT<Parameters>::m_bOpenChildMaximized = true;
+
+typedef CChildFrameExT<> CChildFrameEx;
