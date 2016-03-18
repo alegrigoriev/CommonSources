abf3e278a8d4450e999d87d9b3809b868b4757a9
Alex Grigoriev <alegrigoriev@gmail.com>
2016-03-18 08:03:01 -0700

    Add 64 bit functions to KInterlocked

--- Utils/KInterlocked.h
+++ Utils/KInterlocked.h
@@ -9,9 +9,21 @@ inline T InterlockedIncrementT(T * src)
 	return static_cast<T>(InterlockedIncrement(reinterpret_cast<LONG volatile*>(src)));
 }
 
+template <typename T>
+inline T InterlockedIncrement64T(T * src)
+{
+	C_ASSERT(sizeof(__int64) == sizeof(T));
+	return static_cast<T>(InterlockedIncrement64(reinterpret_cast<__int64 volatile*>(src)));
+}
+
 inline LONG InterlockedIncrementT(volatile LONG * src)
 {
 	return InterlockedIncrement(src);
+}
+
+inline __int64 InterlockedIncrement64T(volatile __int64 * src)
+{
+	return InterlockedIncrement64(src);
 }
 
 template <typename T>
@@ -21,9 +33,21 @@ inline T InterlockedDecrementT(T * src)
 	return static_cast<T>(InterlockedDecrement(reinterpret_cast<LONG volatile*>(src)));
 }
 
+template <typename T>
+inline T InterlockedDecrement64T(T * src)
+{
+	C_ASSERT(sizeof(__int64) == sizeof(T));
+	return static_cast<T>(InterlockedDecrement64(reinterpret_cast<__int64 volatile*>(src)));
+}
+
 inline LONG InterlockedDecrementT(volatile LONG * src)
 {
 	return InterlockedDecrement(src);
+}
+
+inline __int64 InterlockedDecrement64T(volatile __int64 * src)
+{
+	return InterlockedDecrement64(src);
 }
 
 template <typename T, typename T1>
@@ -74,6 +98,54 @@ inline T InterlockedCompareExchangeT(T * src, T1 exchange, T2 compare)
 											reinterpret_cast<LONG volatile &>(compare)));
 }
 
+template <typename T, typename T1>
+inline T InterlockedExchange64T(T * src, T1 exchange)
+{
+	__int64 tmp;
+	return reinterpret_cast<T &>
+			(tmp = InterlockedExchange(
+										const_cast<__int64 *>(& reinterpret_cast<__int64 volatile &>(*src)),
+										reinterpret_cast<__int64 volatile &>(exchange)));
+}
+
+template <typename T>
+inline T InterlockedExchangeAdd64T(T * src, __int64 addend)
+{
+	return static_cast<T>
+			(InterlockedExchangeAdd64(
+									const_cast<__int64 *>(& reinterpret_cast<__int64 volatile &>(*src)),
+									addend));
+}
+
+template <typename T>
+inline T InterlockedAdd64T(T * src, __int64 addend)
+{
+	return static_cast<T>
+			(addend + InterlockedExchangeAdd64(
+												const_cast<__int64 *>(& reinterpret_cast<__int64 volatile &>(*src)),
+												addend));
+}
+
+template <typename T>
+inline T InterlockedSubtract64T(T * src, __int64 addend)
+{
+	return static_cast<T>
+			(InterlockedExchangeAdd64(
+									const_cast<__int64 *>(& reinterpret_cast<__int64 volatile &>(*src)),
+									-addend) - addend);
+}
+
+template <typename T, typename T1, typename T2>
+inline T InterlockedCompareExchange64T(T * src, T1 exchange, T2 compare)
+{
+	__int64 tmp;
+	return reinterpret_cast<T&>
+			(tmp = InterlockedCompareExchange64(
+												const_cast<__int64 *>(& reinterpret_cast<__int64 volatile &>(*src)),
+												reinterpret_cast<__int64 volatile &>(exchange),
+												reinterpret_cast<__int64 volatile &>(compare)));
+}
+
 template <typename T>
 inline T InterlockedCompareExchangePointerT(T * src, PVOID exchange, PVOID compare)
 {
@@ -82,8 +154,6 @@ inline T InterlockedCompareExchangePointerT(T * src, PVOID exchange, PVOID compa
 												exchange,
 												compare));
 }
-
-#define StallExecutionMicrosec KeStallExecutionProcessor
 
 // the function returns previous contents of the memory location being modified
 template <typename T>
@@ -98,6 +168,17 @@ static T InterlockedIncrementModulo(T * src, LONG modulo)
 	return tmp;
 }
 
+template <typename T>
+static T InterlockedIncrement64Modulo(T * src, __int64 modulo)
+{
+	T tmp;
+	do
+	{
+		tmp = *src;
+	}
+	while (tmp != InterlockedCompareExchange64T<T>(src, T(tmp + 1) % T(modulo), tmp));
+	return tmp;
+}
 // the function returns previous contents of the memory location being modified
 template <typename T>
 static T InterlockedAddModulo(T * src, LONG Addend, LONG modulo)
@@ -111,9 +192,20 @@ static T InterlockedAddModulo(T * src, LONG Addend, LONG modulo)
 	return tmp;
 }
 
+template <typename T>
+static T InterlockedAdd64Modulo(T * src, __int64 Addend, __int64 modulo)
+{
+	T tmp;
+	do
+	{
+		tmp = *src;
+	}
+	while (tmp != InterlockedCompareExchange64T<T>(src, T(tmp + Addend) % T(modulo), tmp));
+	return tmp;
+}
 // the function returns previous contents of the memory location being modified
 template <typename T>
-static T InterlockedOrT(T * src, LONG operand)
+static T InterlockedOrT(T * src, T operand)
 {
 	T tmp;
 	do
@@ -125,8 +217,20 @@ static T InterlockedOrT(T * src, LONG operand)
 }
 
 template <typename T>
+static T InterlockedOr64T(T * src, T operand)
+{
+	T tmp;
+	do
+	{
+		tmp = *src;
+	}
+	while (tmp != InterlockedCompareExchange64T<T>(src, tmp | operand, tmp));
+	return tmp;
+}
+
 // the function returns previous contents of the memory location being modified
-static T InterlockedAndT(T * src, LONG operand)
+template <typename T>
+static T InterlockedAndT(T * src, T operand)
 {
 	T tmp;
 	do
@@ -137,9 +241,21 @@ static T InterlockedAndT(T * src, LONG operand)
 	return tmp;
 }
 
+template <typename T>
+static T InterlockedAnd64T(T * src, T operand)
+{
+	T tmp;
+	do
+	{
+		tmp = *src;
+	}
+	while (tmp != InterlockedCompareExchange64T<T>(src, tmp & operand, tmp));
+	return tmp;
+}
+
 // the function returns previous contents of the memory location being modified
 template <typename T>
-static T InterlockedXorT(T * src, LONG operand)
+static T InterlockedXorT(T * src, T operand)
 {
 	T tmp;
 	do
@@ -150,8 +266,20 @@ static T InterlockedXorT(T * src, LONG operand)
 	return tmp;
 }
 
-template<typename T>
-struct NUM_volatile
+template <typename T>
+static T InterlockedXor64T(T * src, T operand)
+{
+	T tmp;
+	do
+	{
+		tmp = *src;
+	}
+	while (tmp != InterlockedCompareExchange64T<T>(src, tmp ^ operand, tmp));
+	return tmp;
+}
+
+template<typename T, int sizeofT = sizeof(T)>
+									struct NUM_volatile
 {
 	T volatile num;
 
@@ -244,6 +372,103 @@ struct NUM_volatile
 	T AddModulo(T Addend, T modulo)
 	{
 		return InterlockedAddModulo<typename T volatile>( & num, Addend, modulo);
+	}
+};
+
+template<typename T>
+struct NUM_volatile<T, 8>
+{
+	T volatile num;
+
+	NUM_volatile(T init=0)
+		: num(init)
+	{
+	}
+	NUM_volatile & operator =(T src)
+	{
+		num = src;
+		return *this;
+	}
+	operator T() const
+	{
+		return num;
+	}
+
+	T operator ++()
+	{
+		return InterlockedIncrement64T<typename T volatile>( & num);
+	}
+
+	T operator ++(int)
+	{
+		return InterlockedIncrement64T<typename T volatile>( & num) - 1;
+	}
+
+	T operator --()
+	{
+		return InterlockedDecrement64T<typename T volatile>( & num);
+	}
+
+	T operator --(int)
+	{
+		return InterlockedDecrement64T<typename T volatile>( & num) + 1;
+	}
+
+	T operator +=(T op)
+	{
+		return InterlockedAdd64T<typename T volatile>(& num, op);
+	}
+	T operator -=(T op)
+	{
+		return InterlockedSubtract64T<typename T volatile>(& num, op);
+	}
+
+	T operator |=(T op)
+	{
+		return InterlockedOr64T<typename T volatile>(& num, op) | op;
+	}
+
+	T Exchange_Or(T op)
+	{
+		return InterlockedOr64T<typename T volatile>(& num, op);
+	}
+
+	T operator &=(T op)
+	{
+		return InterlockedAnd64T<typename T volatile>(& num, op) & op;
+	}
+
+	T Exchange_And(T op)
+	{
+		return InterlockedAnd64T<typename T volatile>(& num, op);
+	}
+
+	T operator ^=(T op)
+	{
+		return InterlockedXor64T<typename T volatile>(& num, op) ^ op;
+	}
+
+	T Exchange(T src)
+	{
+		return InterlockedExchange64T<typename T volatile>( & num, src);
+	}
+	T ExchangeAdd(T src)
+	{
+		return InterlockedExchangeAdd64T<typename T volatile>( & num, src);
+	}
+	T CompareExchange(T src, T Comperand)
+	{
+		return InterlockedCompareExchange64T<typename T volatile>( & num, src, Comperand);
+	}
+
+	T IncrementModulo(T modulo)
+	{
+		return InterlockedIncrement64Modulo<typename T volatile>( & num, modulo);
+	}
+
+	T AddModulo(T Addend, T modulo)
+	{
+		return InterlockedAdd64Modulo<typename T volatile>( & num, Addend, modulo);
 	}
 };
 
